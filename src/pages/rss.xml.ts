48f8f7ab9303c1a855102eec69efe8fe23cb28be
龙一 <46670292+linglilongyi@users.noreply.github.com>
2025-04-19 04:49:36 +0800

    feat: generate feed in atom format - delete rss realated files - change the name of feed at footer - add atom feed's utils

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,52 +1,11 @@
-import rss from "@astrojs/rss";
-import { SITE_DESCRIPTION, SITE_LANGUAGE, SITE_TAB, SITE_TITLE } from "@config";
-import { getCollection } from "astro:content";
-import { marked } from "marked";
+// source: https://github.com/gsong/personal-site/blob/main/src/pages/atom.xml.ts
+import type { APIContext } from "astro";
 
-export async function GET(context: any) {
-  const all_blog_posts = await getCollection("posts");
-  const posts = all_blog_posts.filter((post) => !post.data.draft);
-  const sortedPosts = posts.sort((a: any, b: any) => new Date(b.data.publishDate).getTime() - new Date(a.data.publishDate).getTime());
+import { generateFeed } from "@/utils/feeds";
 
-  function replacePath(content: string, siteUrl: string): string {
-    return content.replace(/(src|img|r|l)="([^"]+)"/g, (match, attr, src) => {
-      if (!src.startsWith("http") && !src.startsWith("//") && !src.startsWith("data:")) {
-        return `${attr}="${new URL(src, siteUrl).toString()}"`;
-      }
-      return match;
-    });
-  }
-
-  const items = await Promise.all(sortedPosts.map(async (posts: any) => {
-    const { data: { title, description, publishDate }, body, slug } = posts;
-
-    const content = body
-      ? replacePath(await marked(body), context.site)
-      : "No content available.";
-
-    const postURL = new URL(`/posts/${slug}/`, context.site);
-
-    return {
-      title,
-      description,
-      link: postURL.toString(),
-      guid: postURL.toString(),
-      content: content,
-      customData: `
-        <dc:creator><![CDATA[${SITE_TAB}]]></dc:creator>
-        <pubDate>${new Date(publishDate).toUTCString()}</pubDate>
-      `,
-    };
-  }));
-
-  return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
-    site: context.site,
-    stylesheet: '/rss.xsl',
-    items,
-    customData: `
-      <language>${SITE_LANGUAGE}</language>
-    `
+export async function GET(context: APIContext) {
+  const feed = await generateFeed(context);
+  return new Response(feed.atom1(), {
+    headers: { "Content-Type": "application/atom+xml" },
   });
 }
